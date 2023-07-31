c97aff6d74824fea5b88d3b5c3af6fd74013e957
Risha Patel <rishapatel78@gmail.com>
2023-07-31 16:22:06 -0500

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by RISHA SANDEEP PATEL on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
